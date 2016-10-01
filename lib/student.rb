4c20b5cec449fce3341e737855c2bd202bd122e9
Jess <jess.harrelson0707@gmail.com>
2016-10-01 16:45:54 -0400

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user.rb'
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+    knowledge
+    #knowledge might need to change to @knowlege array
+    #check this
+  end
 
 end
